faf7031c679972db15006efff3623517d755ae34
Edwin <miguepinchao1992@gmail.com>
2022-11-23 11:42:03 -0600

    Date fecha de nacimiento

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+import "../App.css"
+export default function Navbar() {
+    return (
+        <nav className="navigation">
+          <h1 className="brand-name"  >Calculadora de edad</h1>
+      </nav> )
+  }
